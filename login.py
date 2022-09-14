2ff7fe01b7eb5774f689c322f3a657f4b1ae9f3b
ElvinChiew <elvin_chiew@hotmail.com>
2022-09-14 22:34:01 +0800

    adding draft for user login

---
+++ login.py
@@ -0,0 +1,14 @@
+print("Enter your username and Password:")
+print("Username: ")
+username=str(input())
+print("Password: ")
+password=str(input())
+
+if (username=="elvin") & (password=='1234'):
+    print("login successful")
+else :
+    print("login failed")
+
+#provide 2 text box for username and password
+# a button for login
+# a title /caption "Staff Login"
